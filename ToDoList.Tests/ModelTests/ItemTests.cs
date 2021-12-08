8db1f8a72d33b6e0ef3cc95aae5eee9ada6a732d
Liz Thomas <thomas.elizabeth.k@gmail.com>
2021-12-07 20:43:16 -0800

    Pass test 2

--- ToDoList.Tests/ModelTests/ItemTests.cs
+++ ToDoList.Tests/ModelTests/ItemTests.cs
@@ -9,16 +9,22 @@ namespace ToDoList.Tests
     [TestMethod]
     public void ItemConstructor_CreatesInstanceOfItem_Item()
     {
-      Item newItem = new Item();
+      Item newItem = new Item("test");
       Assert.AreEqual(typeof(Item), newItem.GetType());
     }
-
     [TestMethod]
-    public void GetDescription_ReturnDescription_String()
+    public void GetDescription_ReturnsDescription_String()
     {
+      // Arrange
+      // 1. Create new string called description.
       string description = "Walk the dog.";
+      // 2. Create new Item object, passing in the description.
       Item newItem = new Item(description);
+      // Act
+      // 3. Record the result of retrieving the description property of newItem.
       string result = newItem.Description;
+      // Assert
+      // 4. Confirm the description retrieved from the Item object matches the description string provided to the constructor.
       Assert.AreEqual(description, result);
     }
   }
