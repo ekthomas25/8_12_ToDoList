613b516534689bd2ad0857718874d051ad100eca
Liz Thomas <thomas.elizabeth.k@gmail.com>
2021-12-07 20:43:58 -0800

    Add auto-implemented properties with get

--- ToDoList/Models/Item.cs
+++ ToDoList/Models/Item.cs
@@ -2,6 +2,10 @@ namespace ToDoList.Models
 {
   public class Item
   {
-    //properties, methods, etc. will go here.
+    public string Description { get; }
+    public Item(string description)
+    {
+      Description = description;
+    }
   }
 }
