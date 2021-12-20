f88b375bef852ded6d082706ccbd196f7a26ad19
Liz Thomas <thomas.elizabeth.k@gmail.com>
2021-12-19 17:23:31 -0800

    Update file

--- ToDoList/Controllers/HomeController.cs
+++ ToDoList/Controllers/HomeController.cs
@@ -2,14 +2,50 @@ using Microsoft.AspNetCore.Mvc;
 using ToDoList.Models;
 using System.Collections.Generic;
 
-namespace TodoList.Controllers
+namespace ToDoList.Controllers
 {
   public class HomeController : Controller
   {
+
     [HttpGet("/")]
     public ActionResult Index()
     {
       return View();
     }
+
+    [Route("/favorite_photos")]
+    public ActionResult FavoritePhotos()
+    {
+      return View();
+    }
   }
 }
+
+// using Microsoft.AspNetCore.Mvc;
+// using ToDoList.Models;
+
+// namespace ToDoList.Controllers
+// {
+//   public class HomeController : Controller
+//   {
+//     [HttpGet("/")]
+//     public ActionResult Index() // output: Item.cs
+//     {
+//       Item starterItem = new Item("Add first item to To Do List");
+//       return View(starterItem);
+//     }
+
+//     [HttpGet("/items/new")]
+//     public ActionResult CreateForm()
+//     {
+//       return View(); //renders CreateForm.cshtml automatically
+//     }
+
+//     [HttpPost("/items")] // invoked when form is submitted | matches specified action in CreateForm.cshtml
+//     public ActionResult Create(string description) // description attribute comes from form field submission
+//     {
+//       Item myItem = new Item(description);
+//       return View("Index", myItem); // first argument: view(html) that should be returned, second arg: specifies which iteration of which Model should be displayed. 
+//     }
+//   }
+// }
