ec92795b49cffedc653930e5ee4eee2c9d135625
Liz Thomas <thomas.elizabeth.k@gmail.com>
2021-12-19 21:28:48 -0800

    Add Save() to Create()

--- ToDoList/Controllers/CategoriesController.cs
+++ ToDoList/Controllers/CategoriesController.cs
@@ -1,3 +1,4 @@
+
 using System.Collections.Generic;
 using System;
 using Microsoft.AspNetCore.Mvc;
@@ -7,6 +8,7 @@ namespace ToDoList.Controllers
 {
   public class CategoriesController : Controller
   {
+
     [HttpGet("/categories")]
     public ActionResult Index()
     {
@@ -46,6 +48,7 @@ namespace ToDoList.Controllers
       Dictionary<string, object> model = new Dictionary<string, object>();
       Category foundCategory = Category.Find(categoryId);
       Item newItem = new Item(itemDescription);
+      newItem.Save();
       foundCategory.AddItem(newItem);
       List<Item> categoryItems = foundCategory.Items;
       model.Add("items", categoryItems);
