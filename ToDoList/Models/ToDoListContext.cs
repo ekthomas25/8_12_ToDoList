a8c74265e659397645eb9175c7392eb8fa3eec91
Liz Thomas <thomas.elizabeth.k@gmail.com>
2021-12-20 21:41:52 -0800

    Add file to project

---
+++ ToDoList/Models/ToDoListContext.cs
@@ -0,0 +1,12 @@
+using Microsoft.EntityFrameworkCore;
+using ToDoList.Models;
+
+namespace ToDoList.Models
+{
+  public class ToDoListContext : DbContext // Our ToDoListContext class inherits, or extends, from Entity Framework's DbContext. This ensures it includes all default built-in DbContext functionality.
+  {
+    public DbSet<Item> Items { get; set; } // contains a property of type DbSet named Items that represents the Items table in our ToDoList database and lets us interact with it. DbSet needs to know what C# object it’s going to represent, so we must include Item in the angle brackets (<>) after DbSet.
+
+    public ToDoListContext(DbContextOptions options) : base(options) { } // This constructor inherits the behavior of its parent class constructor. ToDoListContext is an extension of the DbContext class, we're invoking some constructor behavior from that class. We pass a variable of DbContext Options called options to our constructor, instantiating ToDoListContext w/ the options we defined in Startup.cs - dependency injection.
+  }
+}
