59f9168b0a09fc4bfef9e8345ba275f23e04f0e6
Liz Thomas <thomas.elizabeth.k@gmail.com>
2021-12-21 18:35:21 -0800

    Refactor class

--- ToDoList/Models/Category.cs
+++ ToDoList/Models/Category.cs
@@ -10,6 +10,7 @@ namespace ToDoList.Models
     }
     public int CategoryId { get; set; }
     public string Name { get; set; }
-    public virtual ICollection<Item> Items {get; set; } // public property that will return all Items that belong to a category.
+    public virtual ICollection<Item> Items {get; set; } // public property that will return all Items that belong to a category. It is an instance of ICollection, a generic interface built into .NET framework (a collection of method signatures bundled together). Interfaces are often likened to "contracts" the developer "signs" because whenever a class extends an interface it must include every method outlined in the interface. ICollection is specifically a generic interface, which means it contains a bundle of different methods meant to work on a generic collection. We use it specifically because Entity requires it. 
+    // By declaring Items as an ICollection<Item> data type, we're ensuring Entity will be able to use all the ICollection methods it requires on the Item objects in order to act as our ORM.
   }
 }
