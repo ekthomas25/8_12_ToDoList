33f097d53af96e3808870fa5f3e9c84b0632b8a3
Liz Thomas <thomas.elizabeth.k@gmail.com>
2022-01-06 21:37:43 -0800

    Update with collection navigation property

--- ToDoList/Models/Category.cs
+++ ToDoList/Models/Category.cs
@@ -6,11 +6,13 @@ namespace ToDoList.Models
   {
     public Category()
     {
-      this.Items = new HashSet<Item>(); // HashSet = unordered collection of unique elements. We create a HashSet of Items in the constructor to help avoid exceptions when no records exist in the "many" side of the relationship. Also is more performant than a list. It can't have duplicates.
+      this.JoinEntities = new HashSet<Item>(); // HashSet = unordered collection of unique elements. We create a HashSet of Items in the constructor to help avoid exceptions when no records exist in the "many" side of the relationship. Also is more performant than a list. It can't have duplicates.
     }
     public int CategoryId { get; set; }
     public string Name { get; set; }
-    public virtual ICollection<Item> Items {get; set; } // public property that will return all Items that belong to a category. It is an instance of ICollection, a generic interface built into .NET framework (a collection of method signatures bundled together). Interfaces are often likened to "contracts" the developer "signs" because whenever a class extends an interface it must include every method outlined in the interface. ICollection is specifically a generic interface, which means it contains a bundle of different methods meant to work on a generic collection. We use it specifically because Entity requires it. 
+    public virtual ICollection<CategoryItem> JoinEntities {get; set; } 
+    //Collection Navigation property - when you establish a many2many relationship between classes, we need to include this. It is a property on one class that includes a reference to a related class. Contains reference to many related Items. If don't have, can't access related Items in controllers and views.
+    // public property that will return all Items that belong to a category. It is an instance of ICollection, a generic interface built into .NET framework (a collection of method signatures bundled together). Interfaces are often likened to "contracts" the developer "signs" because whenever a class extends an interface it must include every method outlined in the interface. ICollection is specifically a generic interface, which means it contains a bundle of different methods meant to work on a generic collection. We use it specifically because Entity requires it. 
     // By declaring Items as an ICollection<Item> data type, we're ensuring Entity will be able to use all the ICollection methods it requires on the Item objects in order to act as our ORM.
   }
 }
